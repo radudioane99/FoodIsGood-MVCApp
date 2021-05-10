b218e5f608820c280b69f78ff710744a6b4cf1b6
adrianaliasb <scoalaadilaur@gmail.com>
2021-05-10 12:06:58 +0300

    I've added an additional page where you can see the orders for your own offer. You can ACCEPT the offer (but cannot yet delete it, this is easy to implement since it's the innate function, but I have to push now to check things with Bossu' Radu) which will changed the Order.Accepted value from FALSE to TRUE and subtract the quantity from the offer.

---
+++ foodisgood/foodisgood/Migrations/202105091511215_OrderAccepted.cs
@@ -0,0 +1,18 @@
+﻿namespace foodisgood.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class OrderAccepted : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Orders", "Accepted", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Orders", "Accepted");
+        }
+    }
+}
