79856e4fa5e8e310cd87a21218df0dc7424c12b3
adrianaliasb <scoalaadilaur@gmail.com>
2021-05-10 16:11:06 +0300

    [CT] Fixed, added DioaneTest, Dioane e frumuselu' meu wai!!! XOXOXO

---
+++ foodisgood/foodisgood/Migrations/202105101306148_DioaneTest.cs
@@ -0,0 +1,16 @@
+﻿namespace foodisgood.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DioaneTest : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
